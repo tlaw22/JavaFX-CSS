a77117ee9cdaf8891ca4c6440df5a2e3b8666ec9
Tim Lawless <timothy.j.lawless@outlook.com>
2023-01-24 14:15:59 -0500

    finished - webview does not function

--- src/main/java/com/tlaw/javafxcss/HelloApplication.java
+++ src/main/java/com/tlaw/javafxcss/HelloApplication.java
@@ -8,16 +8,17 @@ import javafx.stage.Stage;
 import java.io.IOException;
 
 public class HelloApplication extends Application {
-    @Override
-    public void start(Stage stage) throws IOException {
-        FXMLLoader fxmlLoader = new FXMLLoader(HelloApplication.class.getResource("hello-view.fxml"));
-        Scene scene = new Scene(fxmlLoader.load(), 320, 240);
-        stage.setTitle("Hello!");
-        stage.setScene(scene);
-        stage.show();
-    }
+      @Override
+      public void start(Stage stage) throws IOException {
+            FXMLLoader fxmlLoader = new FXMLLoader(HelloApplication.class.getResource("sample.fxml"));
+            setUserAgentStylesheet(STYLESHEET_CASPIAN);
+            Scene scene = new Scene(fxmlLoader.load(), 500, 240 );
+            stage.setTitle("Style Sheets");
+            stage.setScene(scene);
+            stage.show();
+      }
 
-    public static void main(String[] args) {
-        launch();
-    }
+      public static void main(String[] args) {
+            launch();
+      }
 }
--- src/main/java/com/tlaw/javafxcss/HelloController.java
+++ src/main/java/com/tlaw/javafxcss/HelloController.java
@@ -1,14 +1,73 @@
 package com.tlaw.javafxcss;
 
 import javafx.fxml.FXML;
+import javafx.scene.control.Button;
 import javafx.scene.control.Label;
+import javafx.scene.effect.DropShadow;
+import javafx.scene.layout.GridPane;
+import javafx.stage.DirectoryChooser;
+import javafx.stage.FileChooser;
+
+import java.io.File;
+import java.io.IOException;
+import java.net.URI;
+import java.net.URISyntaxException;
+import java.util.List;
+
+import static jdk.jpackage.internal.WixAppImageFragmentBuilder.ShortcutsFolder.Desktop;
+
 
 public class HelloController {
-    @FXML
-    private Label welcomeText;
+      @FXML
+      private Label label;
+public void initialize(){
 
-    @FXML
-    protected void onHelloButtonClick() {
-        welcomeText.setText("Welcome to JavaFX Application!");
-    }
+            button4.setEffect(new DropShadow());
+}
+@FXML
+      public void handleMouseEnter(){
+      label.setScaleX(2.0);
+      label.setScaleY(2.0);
+}
+@FXML
+      public void handleMouseExit(){
+      label.setScaleX(1.0);
+      label.setScaleY(1.0);
+}
+@FXML
+      private Button button4;
+@FXML
+      public void handleClick(){
+      FileChooser chooser = new FileChooser();
+      chooser.setTitle("Save Application File");
+      chooser.getExtensionFilters().addAll(
+            new FileChooser.ExtensionFilter("Text", "*.txt"),
+            new FileChooser.ExtensionFilter("PDF", "*.pdf"),
+            new FileChooser.ExtensionFilter("Images", "*.png", "*.jpg", "*.gif"),
+            new FileChooser.ExtensionFilter("ALL", "*.*")
+      );
+      List<File> file = chooser.showOpenMultipleDialog(gridPane.getScene().getWindow());
+      if(file != null) {
+            for(int i=0; i<file.size(); i++) {
+                  System.out.println(file.get(i));
+            }
+//            System.out.println(file.getPath());
+      } else {
+            System.out.println("Chooser was cancelled");
+      }
+}
+@FXML
+      private GridPane gridPane;
+@FXML
+      public void handleLinkClicked(){
+      //        try {
+//            Desktop.getDesktop().browse(new URI("http://www.javafx.com"));
+//        } catch(IOException e) {
+//            e.printStackTrace();
+//        } catch(URISyntaxException e) {
+//            e.printStackTrace();
+//        }
+      WebEngine engine = webView.getEngine();
+      engine.load("http://www.javafx.com");
+}
 }
